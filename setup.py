dbca0ed36384da045bb0e4ffd382fccb8c39c6eb
lordflaron <amichuda@gmail.com>
2018-12-11 16:26:36 -0800

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='Stata Do-file Exporter',
-    version='0.1',
+    version='0.1.1',
     packages=['statadoexport'],
     author='Aleksandr Michuda',
     author_email='amichuda@gmail.com',
     description='An nbconvert exporter from .ipynb to .do',
     url='https://github.com/lordflaron/StataDoExport',
+    license='LICENSE',
     long_description = open('README.rst').read(),
     install_requires=[
         "nbconvert"
